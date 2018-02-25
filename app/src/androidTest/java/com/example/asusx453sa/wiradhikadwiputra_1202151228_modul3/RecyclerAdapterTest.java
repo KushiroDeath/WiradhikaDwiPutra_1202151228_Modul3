3c44e5dd150a020277f1674d5147e6df69eda940
KushiroDeath <wiradd@yahoo.com>
2018-02-25 16:32:41 +0700

    first commit

---
+++ app/src/androidTest/java/com/example/asusx453sa/wiradhikadwiputra_1202151228_modul3/RecyclerAdapterTest.java
@@ -0,0 +1,21 @@
+package com.example.asusx453sa.wiradhikadwiputra_1202151228_modul3;
+
+import org.junit.Test;
+
+/**
+ * Created by ASUS X453SA on 2/23/2018.
+ */
+
+public class RecyclerAdapterTest {
+    @Test
+    public void onCreateViewHolder() throws Exception {
+    }
+
+    @Test
+    public void onBindViewHolder() throws Exception {
+    }
+
+    @Test
+    public void getItemCount() throws Exception {
+    }
+}
